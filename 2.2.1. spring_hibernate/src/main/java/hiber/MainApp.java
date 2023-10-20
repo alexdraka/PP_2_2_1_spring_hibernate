6706ce5b1760bd4ac937d163e66cb1a0596cc085
alexdraka <604.syndrome@gmail.com>
2023-10-20 16:47:26 +0300

    first commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,10 +1,11 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
+import hiber.service.UserServiceImp;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 import java.sql.SQLException;
 import java.util.List;
 
@@ -15,19 +16,13 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User5", "Lastname5", "user5@mail.ru", new Car("model1", 1234567890)));
+      userService.add(new User("User6", "Lastname6", "user6@mail.ru", new Car("model2", 1234567891)));
+      userService.add(new User("User7", "Lastname7", "user7@mail.ru", new Car("model3", 1234567892)));
+      userService.add(new User("User8", "Lastname8", "user8@mail.ru", new Car("model4", 1234567893)));
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+      System.out.println(userService.listUsers());
+      System.out.println(userService.getUserByCar("model2", 1234567891));
 
       context.close();
    }
